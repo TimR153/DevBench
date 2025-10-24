55d7ec4e8c53a45566560fee5172605bac966bfd
Tim Rentrop <timrentrop@proton.me>
2025-10-24 19:35:30 +0200

    DarkMode eingebaut.

--- DevBench/Program.cs
+++ DevBench/Program.cs
@@ -1,15 +1,15 @@
-using DevBench.Client.Pages;
 using DevBench.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
 
+builder.Services.AddMudServices();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
@@ -17,7 +17,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
